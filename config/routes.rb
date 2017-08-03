3e3c39a2c0465525eee1a510881dfee2c9ecb7af
wildpow <element1028@gmail.com>
2017-08-02 21:56:39 -0700

    Complete Guest book app from Chapter 4
    Setup a db table for names entered in the ‘entries/sign_in’ view and
    also setup a controller to display db entries in same view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'entries/sign_in' => 'entries#sign_in'
+  post 'entries/sign_in' => 'entries#sign_in'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
